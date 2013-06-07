0c349e22515a3eba484a2046a2af0e89caabbd39
Takashi SUGA <suchowan@box.email.ne.jp>
2013-06-07 19:50:44 +0900

    correct year note of Bahai calendar

--- lib/when_exe/region/bahai.rb
+++ lib/when_exe/region/bahai.rb
@@ -49,7 +49,7 @@ module When
       "names:[Bahai]",
 
       [Residue,
-        "label:[YearName]", "divisor:19", "year:1",
+        "label:[YearName]", "divisor:19", "year:0", "format:[%1$s(%3$d)]",
         [Residue, "label:[Alif=,   ألف=,   A=            ]", "remainder:  0"],
         [Residue, "label:[Bá=,     باء=,   B=            ]", "remainder:  1"],
         [Residue, "label:[Ab=,     أب=,    Father=       ]", "remainder:  2"],
