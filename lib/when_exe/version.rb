d3ea396734c29dcb1c6bd8571e99c31b6b5f7e16
Takashi SUGA <suchowan@box.email.ne.jp>
2015-02-19 16:25:08 +0900

    Addition of Copyright string

--- lib/when_exe/version.rb
+++ lib/when_exe/version.rb
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 =begin
-  Copyright (C) 2013-2015 Takashi SUGA
+  Copyright (C) 2011-2015 Takashi SUGA
 
   You may use and/or modify this file according to the license described in the LICENSE.txt file included in this archive.
 =end
 
 module When
-  VERSION = "0.4.1"
+  VERSION   = "0.4.1"
+  COPYRIGHT = "(C) 2011-2015 Takashi SUGA"
 end
