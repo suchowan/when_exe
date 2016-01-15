d659683423c984218c642f16dd4713940426e46d
suchowan <suchowan@box.email.ne.jp>
2016-01-15 14:43:10 +0900

    Preparation for gem version up

--- lib/when_exe/version.rb
+++ lib/when_exe/version.rb
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 =begin
-  Copyright (C) 2011-2015 Takashi SUGA
+  Copyright (C) 2011-2016 Takashi SUGA
 
   You may use and/or modify this file according to the license described in the LICENSE.txt file included in this archive.
 =end
 
 module When
-  VERSION   = "0.4.4"
-  YEARS     = "(C) 2011-2015"
+  VERSION   = "0.4.5"
+  YEARS     = "(C) 2011-2016"
   AUTHOR    = "Takashi SUGA"
   COPYRIGHT = "#{YEARS} #{AUTHOR}"
 
